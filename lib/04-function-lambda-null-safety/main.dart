ae45bf60f2fecba2c4e68f56297d072244cd82ac
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-05-14 21:43:22 +0200

    function lambda null safety -> Q4

--- lib/04-function-lambda-null-safety/main.dart
+++ lib/04-function-lambda-null-safety/main.dart
@@ -4,12 +4,39 @@ Initiation à Dart
 Functions + Lambdas + Null Safety
 */
 
-//Exercice 1
+void main() {
+  //Q1
+  print(sayHello("John"));
 
+  //Q2
 
-//Exercice 2
+}
 
+//Q1
+sayHello(String to)
+{
+  return "Hello, $to!";
+}
 
-// . . .
+//Q2
+num sum(num n1, num n2)
+{
+  return n1 + n2;
+}
 
+//Q3
+num sumMany(List<num> numbers)
+{
+  num res = 0;
+  numbers.forEach((element) {
+    res += element;
+  });
+  return res;
+}
 
+//Q4
+void sumAndPrint(num n1, num n2, [bool shouldPrint = false])
+{
+  num result = n1 + n2;
+  print((shouldPrint ? "$n1 + $n2 = " : "") + result.toString());
+}
